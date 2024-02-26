6c81769f9101197f1c361cf64295f0400e38efa0
Golfantara <golfantara7@gmail.com>
2024-02-26 14:19:15 +0700

    improvement: Set up Websocket

---
+++ features/websocket/ws_handler.go
@@ -0,0 +1,16 @@
+package websocket
+
+type Handler struct {
+	hub *Hub
+}
+
+func NewHandler(h *Hub) *Handler {
+	return &Handler{
+		hub: h,
+	}
+}
+
+type CreateRoomReq struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
