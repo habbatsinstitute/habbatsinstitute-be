412eb9dc82d1f20fd6c31bfc50fd8f2c485cfbf9
Golfantara <golfantara7@gmail.com>
2024-02-28 13:55:24 +0700

    features: set up websocket

--- utils/websocket/client.go
+++ utils/websocket/client.go
@@ -1,1 +1,33 @@
 package websocket
+
+import (
+	"encoding/base64"
+	"fmt"
+
+	"institute/utils/websocket/packet"
+
+	"github.com/gorilla/websocket"
+	"github.com/labstack/echo/v4"
+)
+
+type Client struct {
+	server  *Server
+	handler *websocket.Conn
+	rooms   map[int]*Room
+	message chan *packet.Message
+	sign    string
+}
+
+func NewClient(context echo.Context, server *Server, user int, role string) (string, *Client) {
+	var (
+		format = fmt.Sprintf("%s@%d", role, user)
+		sign   = base64.RawStdEncoding.EncodeToString([]byte(format))
+	)
+	return format, &Client{
+		server:  server,
+		handler: NewProtocol().Switch(context),
+		rooms:   make(map[int]*Room),
+		message: make(chan *packet.Message),
+		sign:    sign,
+	}
+}
--- utils/websocket/protocol.go
+++ utils/websocket/protocol.go
@@ -1,1 +1,35 @@
 package websocket
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
+)
+
+type Protocol struct {
+	upgrader *websocket.Upgrader
+	dialer   *websocket.Dialer
+}
+
+func NewProtocol() *Protocol {
+	return &Protocol{
+		upgrader: &websocket.Upgrader{
+			ReadBufferSize:  4096,
+			WriteBufferSize: 4096,
+		},
+		dialer: websocket.DefaultDialer,
+	}
+}
+
+func (proto *Protocol) Switch(ctx echo.Context) *websocket.Conn {
+	response, request := ctx.Response(), ctx.Request()
+	header := http.Header{}
+	handler, err := proto.upgrader.Upgrade(response, request, header)
+	if err != nil {
+		logrus.Error("[protocol.switch]: ", err.Error())
+		return nil
+	}
+	return handler
+}
