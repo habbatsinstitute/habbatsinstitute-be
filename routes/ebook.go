1461e8100af29450fb9f7fc2174e81f13878d170
Golfantara <golfantara7@gmail.com>
2024-08-15 10:59:01 +0700

    improvement: add feature update and get all ebook

--- routes/ebook.go
+++ routes/ebook.go
@@ -13,10 +13,10 @@ import (
 func Ebooks(e *echo.Echo, handler ebook.Handler, jwt helpers.JWTInterface, config config.ProgramConfig) {
 	ebooks := e.Group("/ebooks")
 
-	ebooks.GET("", handler.GetEbooks())
+	ebooks.GET("", handler.GetEbooks(), m.AuthorizeJWT(jwt, 3, config.SECRET))
 	ebooks.POST("", handler.CreateEbook(), m.AuthorizeJWT(jwt, 3, config.SECRET))
 	
 	ebooks.GET("/:id", handler.EbookDetails())
-	// ebooks.PUT("/:id", handler.UpdateEbook())
-	ebooks.DELETE("/:id", handler.DeleteEbook())
+	ebooks.PUT("/:id", handler.UpdateEbook(), m.AuthorizeJWT(jwt, 3, config.SECRET))
+	ebooks.DELETE("/:id", handler.DeleteEbook(),)
 }
